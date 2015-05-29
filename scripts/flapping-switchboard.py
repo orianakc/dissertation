0fabf924225090bdda9bd37ecd6ba24c271c7bab
orianakc <entropic.alchemist@gmail.com>
2015-05-28 23:53:39 -0400

    Added finding corresponding phonwords file, extracting orthography, stress profile, and word ID

--- scripts/flapping-switchboard.py
+++ scripts/flapping-switchboard.py
@@ -16,17 +16,25 @@ def makeDict(tree,fileID,csvOut,colNames):
 		print "problem opening %s syllable file" % fileID
 		print e
 
+	try: 
+		wordTree = minidom.parse('/Users/oriana/Corpora/nxt_switchboard_ann/xml/phonwords/'+fileID+'.phonwords.xml')
+	except ValueError as e:
+		print "problem opening %s phonword file" % fileID
+		print e
+
+
 	## Prepare lists of things to search through
 	phs = tree.getElementsByTagName('ph')
 	sylls = syllTree.getElementsByTagName('syllable')
 	syllPhones = syllTree.getElementsByTagName('nite:child')
 	syllHrefs = [s.getAttribute('href') for s in syllPhones]
-	
+	phonwords = wordTree.getElementsByTagName('phonword')
 
 
 	## Pick up all the [t] (and [d]?)
 	tphs = [p for p in phs if p.firstChild.data=='t']
 	dictList = []
+	wdIndex = 0
 	for t in tphs:
 		valDict = {}
 		valDict['fileID'] = fileID
@@ -71,6 +79,23 @@ def makeDict(tree,fileID,csvOut,colNames):
 
 		## Am I in a stressed syllable? p = primary, s=secondary, n=none. Some syllables have no stress attribute.
 
+		## What phonword am I in? 
+		myWord = None
+		for w in phonwords[wdIndex:]:
+			if float(w.getAttribute('nite:start'))<= float(t.getAttribute('nite:start')):
+				if float(w.getAttribute('nite:end')) >= float(t.getAttribute('nite:end')):
+					myWord = w
+					wdIndex = phonwords.index(w)
+				else: 
+					pass
+
+
+		valDict['wordOrth'] = myWord.getAttribute('orth')
+		valDict['wordStress'] = myWord.getAttribute('stressProfile')
+		valDict['wordID'] = myWord.getAttribute('nite:id')
+
+
+
 		# mySyll.getAttribute('msstate') == t.getAttribute('msstate')
 		dictList.append(valDict)
 
@@ -78,7 +103,7 @@ def makeDict(tree,fileID,csvOut,colNames):
 		csvOut.writerow([d[v] for v in header])
 	return dictList
 
-header = ['fileID','msstate','nite:start','nite:end','nite:id','prevSeg','follSeg']
+header = ['fileID','msstate','nite:start','nite:end','nite:id','prevSeg','follSeg','wordOrth','wordStress','wordID']
 
 def xmlExtract(fileName,dataName,colNames):
     with open(dataName, 'wb') as data:
