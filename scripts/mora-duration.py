fd379e2dfe65b2d28f74387d0c1f42a26cc340ed
orianakc <entropic.alchemist@gmail.com>
2015-06-02 20:32:27 -0400

    Updated mora script

--- scripts/mora-duration.py
+++ scripts/mora-duration.py
@@ -4,36 +4,94 @@ from xml.dom import minidom
 import os
 import re
 import csv
+import datetime
 
-fileName = 'A01F0055.xml'#raw_input('What file shall I process for you? : ')
-tree = minidom.parse(fileName)
+# tree = minidom.parse(fileName)
+def makeDict(tree,fname,csvOut,colNames):
+	moras = tree.getElementsByTagName("Mora")
+	# return moras
+	print 'Found %d moras' % len(moras)
+	for mora in moras:
+		valDict = {}
+		moraPhones = mora.getElementsByTagName('Phone')
 
-moras = tree.getElementsByTagName("Mora")
+		if len(moraPhones) == 0:
+			pass
+		else:
+			valDict['file'] = fname
+			valDict['MoraStart'] = moraPhones[0].attributes['PhoneStartTime'].value
+			valDict['MoraEnd'] = moraPhones[-1].attributes['PhoneEndTime'].value
+			valDict['MoraDuration'] = float(valDict['MoraEnd']) - float(valDict['MoraStart'])
+			valDict['MoraEntity'] = mora.getAttribute('MoraEntity').encode('utf-8')
+			dictList.append(valDict)
+			valDict['ID'] = mora
+			dictList.append(valDict)
+	
 
-dictList = []
+# moraNames = [d['MoraEntity'] for d in dictList]
+# moraNamesUnique = list(set(moraNames))
 
-for mora in moras:
-	valDict = {}
-	moraPhones = mora.getElementsByTagName('Phone')
-
-	if len(moraPhones) == 0:
-		pass
-	else:
-		valDict['file'] = fileName
-		valDict['MoraStart'] = moraPhones[0].attributes['PhoneStartTime'].value
-		valDict['MoraEnd'] = moraPhones[-1].attributes['PhoneEndTime'].value
-		valDict['MoraDuration'] = float(valDict['MoraEnd']) - float(valDict['MoraStart'])
-		valDict['MoraEntity'] = mora.getAttribute('MoraEntity').encode('utf-8')
-		dictList.append(valDict)
+# avgMoraDuration = {}
+# for m in moraNamesUnique:
+# 	durs = [d['MoraDuration'] for d in dictList if d['MoraEntity']==m]
+# 	avgMoraDuration[m] = sum(durs)/float(len(durs))
 
 
+
+
+
+outputFile = raw_input('Name of output file: ')
+ #'mora-duration.tdf'
+dataDir = raw_input('Path to XML files : ')
+ #'/Users/oriana/dissertation/data/CSJ/XML/'
+fileName = raw_input('What file(s) shall I process for you? : ')
+# ".*.xml"
 colNames = ['file','MoraStart','MoraEnd','MoraDuration','MoraEntity']
 
-with open('mora-duration.tdf','wb') as data:
-	csvOut = csv.writer(data,delimiter='\t',quotechar='"')
-	csvOut.writerow(colNames)
-	for d in dictList:
+ts = datetime.datetime.now()
+with open(outputFile, 'wb') as data:
+    csvOut = csv.writer(data,delimiter="\t",quotechar='"')
+    csvOut.writerow(colNames)
+    dictList = []
+    for root, dirs, files in os.walk(dataDir):
+        for fname in files:
+            if re.search(fileName, fname):
+                print 'Opening ' + fname + " at " +  str(datetime.datetime.now())
+                try:
+                    tree = minidom.parse(os.path.join(dataDir,fname))
+                    makeDict(tree,fname,csvOut,colNames)
+                except ValueError as e:
+                    print "problem opening %s" % fname
+                    print e           			
+                
+    print 'Writing out CSV file at ' + str(datetime.datetime.now())
+    print 'There will be %d rows' % len(dictList)
+    for d in dictList:
 		csvOut.writerow([d[v] for v in colNames])
+tf = datetime.datetime.now()
+te = tf - ts
+print 'This took %s seconds' % str(te)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
