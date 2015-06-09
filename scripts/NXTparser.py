9a8a4a97fc3d47e676a7dcc2b783a3d9a43deddf
orianakc <entropic.alchemist@gmail.com>
2015-06-08 20:47:32 -0400

    Added new module NXTparser which can find parent-child relations among xml files

---
+++ scripts/NXTparser.py
@@ -0,0 +1,91 @@
+## Script for putting together all of a file's information.
+## Oriana June 2015
+
+from xml.dom import minidom
+import os
+import re
+import csv
+
+mainTags = {
+	'phones':'ph',
+	'syllables':'syllable',
+	'phonwords':'phonword',
+	'phrases':'phrase'
+}
+
+class Speaker:
+	'One speaker in a dialogue with a unique ID number.'
+
+	def __init__(self, ID, xmlDir):
+		self.ID = ID
+		self.xmlDir = xmlDir
+
+	def makeTree(self, tag):
+		try:
+			xmlFile = '%s%s/%s.%s.xml'%(self.xmlDir, tag, self.ID, tag)
+			tree = minidom.parse(xmlFile)
+			for c in tree.firstChild.childNodes:
+				if c.nodeType == 1:
+					c.setIdAttribute('nite:id')
+			setattr(self,tag+'Tree',tree)
+			nameOfTag = mainTags[tag]
+			setattr(self,tag+'List',tree.getElementsByTagName(nameOfTag))
+		except Exception, e:
+			print "Can't build %s Tree for %s" % (tag, self.ID)
+			raise e
+
+	def getTokens(self,phone):
+		assert hasattr(self,'phonesList'), "Need to create a phonesList first. Try using the makeTree(phones) method."
+		tokenList = [t for t in self.phonesList if t.firstChild.data=='t']
+		setattr(self,phone+'List',tokenList)
+
+# Gets the nodes corresponding to those defined by nite:child
+def getChildren(node,childTree):
+	nitechild = node.getElementsByTagName('nite:child')[0]
+	hrefs = re.search('.*#(.*)',nitechild.getAttribute('href')).groups()[0]
+	idStartEnd = hrefs.split('..')
+	# print idStartEnd
+	childIDs = []
+	if idStartEnd[0]==idStartEnd[-1]:
+		childIDs = re.search('id\((\w*)\)',idStartEnd[0]).groups()[0]
+		# print "Only 1 child : " + " ".join(childIDs)
+	else:
+		assert len(idStartEnd)==2, "Can't find first/last child IDs for %s" % str(node)
+		start = re.search('id\((\w*)\)',idStartEnd[0]).groups()[0]
+		end = re.search('id\((\w*)\)',idStartEnd[1]).groups()[0]
+		startNo = re.search('.*\_..(\d+)',start).groups()[0]
+		endNo = re.search('.*\_..(\d+)',end).groups()[0]
+		prefix = re.search('(.*\_..)\d+',start).groups()[0]
+		for n in range(int(startNo),int(endNo)+1):
+			childIDs.append(prefix+str(n))
+		# print childIDs
+	return childIDs
+
+# class Token:
+# 	"""docstring for Token"""
+# 	def __init__(self, phone, speaker):
+# 		self.phone = phone
+# 		self.speaker = speaker
+
+		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- scripts/flapping-switchboard.py
+++ scripts/flapping-switchboard.py
@@ -6,41 +6,45 @@ from xml.dom import minidom
 import os
 import re
 import csv
+import sys
+sys.path.append("~/dissertation/scripts/")
+from NXTparser import *
 
 inputDir = '/Users/oriana/Corpora/nxt_switchboard_ann/xml/' # This should point to the file containing folders for each of the XML annotation types
 outputDir = '~/dissertation/data/switchboard_nxt/' # Where the output file goes. 
 
-def makeDict(tree,fileID,csvOut,colNames):
-	## Get the files parsed.
-	# phTree = minidom.parse(os.path.join(myPath,'phones/',fname))
-	try: 
-		syllTree = minidom.parse('/Users/oriana/Corpora/nxt_switchboard_ann/xml/syllables/'+fileID+'.syllables.xml')
-	except ValueError as e:
-		print "problem opening %s syllable file" % fileID
-		print e
+def makeDict(speaker,csvOut,colNames):
+	# ## Get the files parsed.
+	# # phTree = minidom.parse(os.path.join(myPath,'phones/',fname))
+	# try: 
+	# 	syllTree = minidom.parse('/Users/oriana/Corpora/nxt_switchboard_ann/xml/syllables/'+fileID+'.syllables.xml')
+	# except ValueError as e:
+	# 	print "problem opening %s syllable file" % fileID
+	# 	print e
 
-	try: 
-		wordTree = minidom.parse('/Users/oriana/Corpora/nxt_switchboard_ann/xml/phonwords/'+fileID+'.phonwords.xml')
-	except ValueError as e:
-		print "problem opening %s phonword file" % fileID
-		print e
-
+	# try: 
+	# 	wordTree = minidom.parse('/Users/oriana/Corpora/nxt_switchboard_ann/xml/phonwords/'+fileID+'.phonwords.xml')
+	# except ValueError as e:
+	# 	print "problem opening %s phonword file" % fileID
+	# 	print e
+	tags = ['phones','syllables','phonwords','phrases']
+	for t in tags:
+		speaker.makeTree(t)
 
 	## Prepare lists of things to search through
-	phs = tree.getElementsByTagName('ph')
-	sylls = syllTree.getElementsByTagName('syllable')
-	syllPhones = syllTree.getElementsByTagName('nite:child')
-	syllHrefs = [s.getAttribute('href') for s in syllPhones]
-	phonwords = wordTree.getElementsByTagName('phonword')
-
+	phs = speaker.phonesList
+	sylls = speaker.syllablesList
+	phonwords = speaker.phonwordsList
 
 	## Pick up all the [t] (and [d]?)
-	tphs = [p for p in phs if p.firstChild.data=='t']
+	speaker.getTokens('t')
+	tphs = speaker.tList
 	dictList = []
 	wdIndex = 0
+	syllIndex = 0
 	for t in tphs:
 		valDict = {}
-		valDict['fileID'] = fileID
+		valDict['fileID'] = speaker.ID
 		for k in t.attributes.keys():
 			valDict[k] = t.getAttribute(k)
 		## Segment duration
@@ -80,27 +84,40 @@ def makeDict(tree,fileID,csvOut,colNames):
 		valDict['follSeg'] = follSeg
 
 		## Find the ID of the syllable I'm in 
-
-
+		for s in sylls[syllIndex:]:
+			childPhones = getChildren(s,speaker.phonesTree)
+			if t.getAttribute('nite:id') in childPhones:
+				valDict['syllableNode'] = s
+				syllIndex = sylls.index(s)
+				print "Found syllable node : " + valDict['syllableNode'].getAttribute('nite:id')
+				break
+			else:
+				continue
+		assert valDict['syllableNode'], "Couldn't find a syllable for "+ t 
 		## Am I in a stressed syllable? p = primary, s=secondary, n=none. Some syllables have no stress attribute.
+		if valDict['syllableNode'].hasAttribute('stress') == False:
+			valDict['stressedSyll'] = 'NA'
+		else:
+			valDict['stressedSyll'] =valDict['syllableNode'].getAttribute('stress')
 
 		## What phonword am I in? 
-		myWord = None
 		for w in phonwords[wdIndex:]:
-			if float(w.getAttribute('nite:start'))<= float(t.getAttribute('nite:start')):
-				if float(w.getAttribute('nite:end')) >= float(t.getAttribute('nite:end')):
-					myWord = w
-					wdIndex = phonwords.index(w)
-				else: 
-					pass
+			childSylls = getChildren(w,speaker.syllablesTree)
+			if valDict['syllableNode'].getAttribute('nite:id') in childSylls:
+				myWord = w
+				wdIndex = phonwords.index(w)
+				valDict['wordOrth'] = myWord.getAttribute('orth')
+				valDict['wordStress'] = myWord.getAttribute('stressProfile')
+				valDict['wordID'] = myWord.getAttribute('nite:id')
+				print 'Found it!: ' + valDict['wordOrth']
+				break
+		# assert type(valDict['wordOrth']) == str, "Couldn't find a word for " + valDict['syllableNode'].getAttribute('nite:id')
 
-		valDict['wordOrth'] = myWord.getAttribute('orth')
-		valDict['wordStress'] = myWord.getAttribute('stressProfile')
-		valDict['wordID'] = myWord.getAttribute('nite:id')
+
 
 		## At word boundary? 		
-		print re.search('(.*)_.*',t.getAttribute('nite:id')).groups() 
-		print re.search('(.*)_.*',phs[phs.index(t)-1].getAttribute('nite:id')).groups()
+		# print re.search('(.*)_.*',t.getAttribute('nite:id')).groups() 
+		# print re.search('(.*)_.*',phs[phs.index(t)-1].getAttribute('nite:id')).groups()
 		if re.search('(.*)_.*',t.getAttribute('nite:id')).groups() == re.search('(.*)_.*',phs[phs.index(t)-1].getAttribute('nite:id')).groups():
 				valDict['wordBoundaryLeft'] = "N"
 		else: 
@@ -129,9 +146,9 @@ def xmlExtract(fileName,dataName,colNames):
                 if re.search(fileName, fname):
 					fileID = re.search(".*(?=\.[a-z]*\.xml)",fname).group()
 					try:
-						tree = minidom.parse(os.path.join(root,fname))
+						speaker = Speaker(fileID,inputDir)
 						print fname
-						makeDict(tree,fileID,csvOut,colNames)
+						makeDict(speaker,csvOut,colNames)
 					except ValueError as e:
 						print "problem opening %s" % fname
 						print e
